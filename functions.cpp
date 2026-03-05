7db7dec764b3eb14271e495409ec5357549d184b
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-03-05 22:33:54 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a , int b){
+        int c = a+b;
+        return c;
+    }
+
+    
+int main(){
+    int num1 , num2;
+    cout << "enter num1:" << endl;
+    cin >> num1;
+    cout << "enter num2 :" << endl;
+    cin >> num2;
+    cout << "sun is :" << sum(num1 , num2);
+
+    return 0;
+}
+
+
+
+
+
+
