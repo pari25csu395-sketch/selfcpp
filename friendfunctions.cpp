f98cb226c2b65b1937b8f206075e65d0c8f32300
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-03-09 09:55:08 +0530

    Add files via upload

---
+++ friendfunctions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+    int a, b;
+    public:
+    void setnumber(int n1 , int n2){
+        a = n1;
+        b = n2;
+    }
+    friend Complex Sumcomplex(Complex o1 , Complex o2); // declaration so that sumcomplex can access data in complex.
+    void printnumber(){
+        cout << " your number is " << a << "+" << b << "i" << endl;
+    }
+    
+};
+
+Complex Sumcomplex(Complex o1 , Complex o2){
+    Complex o3;
+    o3. setnumber((o1.a + o2.a), (o1.b + o2.b));
+    return o3;
+}
+int main() {
+    Complex c1 , c2 , sum;
+    c1.setnumber(1, 5);
+    c1.printnumber();
+
+    c2.setnumber(4 ,2);
+    c2.printnumber();
+
+    sum = Sumcomplex(c1 , c2);
+    sum. printnumber();
+
+return 0;
+}
